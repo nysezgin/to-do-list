b66b08f7d84823fd24c82d8f8bb2534b3ca7ff3a
nysezgin <74066374+nysezgin@users.noreply.github.com>
2021-01-19 20:19:22 +0300

    dark theme added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import "./reset.css";
 import "./App.scss";
+import {useState, useEffect} from "react"
 import Header from "./components/Header";
 import Main from "./components/Main";
 
+const getStorageTheme = () => {
+  let theme = "light-theme";
+  if (localStorage.getItem("theme")) {
+    theme = localStorage.getItem("theme");
+  }
+  return theme;
+};
+
 function App() {
+  const [theme, setTheme] = useState(getStorageTheme());
+
+  const toggleTheme = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+    } else {
+      setTheme("light-theme");
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem("theme", theme);
+  }, [theme]);
   return (
     <div className="App">
-      <Header />
+      <Header theme={theme} toggleTheme={toggleTheme} />
       <Main />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,39 @@
 import React from "react";
-import moon from "./images/icon-moon.svg";
+import moonImg from "./images/icon-moon.svg";
+import sunImg from "./images/icon-sun.svg";
 
-export default function Header() {
+export default function Header({ theme, toggleTheme }) {
   return (
     <header>
-      <h1 className="header__Title">
+      <h1 className="header__title">
         <a href={"/"}>TODO</a>
       </h1>
-      <img src={moon} alt="" />
-      <div className="header__background"/>
+      <button className="header__toggle-wrapper" onClick={() => toggleTheme()}>
+        <p className="header__toggle-name">
+          {theme === "light-theme" ? "Dark Theme" : "Light Theme"}
+        </p>
+        <div className="header__toggle-image-wrapper" onClick={() => toggleTheme()}>
+          <img
+            className={
+              theme === "light-theme"
+                ? "header__toggle-image"
+                : "header__toggle-image header__toggle-image--deactive"
+            }
+            src={moonImg}
+            alt=""
+          />
+          <img
+            className={
+              theme === "dark-theme"
+                ? "header__toggle-image"
+                : "header__toggle-image header__toggle-image--deactive"
+            }
+            src={sunImg}
+            alt=""
+          />
+        </div>
+      </button>
+      <div className="header__background" />
     </header>
   );
 }
